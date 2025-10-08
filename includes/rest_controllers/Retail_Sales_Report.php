314e3749718826668fa07f1c4dc2c4085e53d2d7
junaidbinjaman <me@junaidbinjaman.com>
2025-10-08 20:54:40 +0600

    feat: Retail sales report api is now delivering data but not filtering them based on user role. So will develop that filter up next.

---
+++ includes/rest_controllers/Retail_Sales_Report.php
@@ -0,0 +1,7 @@
+<?php
+
+namespace includes\rest_controllers;
+
+class Retail_Sales_Report {
+
+}
--- react-ui/src/Admin.tsx
+++ react-ui/src/Admin.tsx
@@ -1,10 +1,12 @@
 import SalseReportByUserRole from "@/components/salseReportByUserRole";
+import Example from "@/components/example.tsx";
 
 function Admin() {
 
     return (
         <div>
             <SalseReportByUserRole />
+            <Example />
         </div>
     )
 }
